4c74e39a646ad1fbf188d133cdbf57a39d99947f
Jad AlHamwi <j.hamwi@centroidsol.com>
2025-01-08 21:02:09 +0400

    compose update

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 USERNAME=$1
 PASSWORD=$2
-DB_URL="mongodb://$USERNAME:$PASSWORD@offerly-db:27017/offerly"
-DATA_DIR='/offerly-data'
-UPLOADS_DIR='/offerly-uploads'
+export DB_URL="mongodb://$USERNAME:$PASSWORD@offerly-db:27017/offerly"
+export DATA_DIR='/offerly-data'
+export UPLOADS_DIR='/offerly-uploads'
 COUNTRIES='["Saudi Arabia"]'
 CATEGORIES='["Shopping","Travel","Restaurants & Cafes","Entertainment","Car Services","Health & Wellness","Others","Groceries"]'
 LANGUAGES='["ar","en"]'
