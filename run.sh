b579f91d1f669a8d6ac015a9acea9a811b85a4ca
Jad AlHamwi <j.hamwi@centroidsol.com>
2025-01-08 21:05:19 +0400

    run script update

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@ PASSWORD=$2
 export DB_URL="mongodb://$USERNAME:$PASSWORD@offerly-db:27017/offerly"
 export DATA_DIR='/offerly-data'
 export UPLOADS_DIR='/offerly-uploads'
-COUNTRIES='["Saudi Arabia"]'
-CATEGORIES='["Shopping","Travel","Restaurants & Cafes","Entertainment","Car Services","Health & Wellness","Others","Groceries"]'
-LANGUAGES='["ar","en"]'
+export COUNTRIES='["Saudi Arabia"]'
+export CATEGORIES='["Shopping","Travel","Restaurants & Cafes","Entertainment","Car Services","Health & Wellness","Others","Groceries"]'
+export LANGUAGES='["ar","en"]'
+export NODE_ENV=production
 sh ./pull.sh
 docker compose up --build -d
