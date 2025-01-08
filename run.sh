75eaecaaad0d782148a3665ed20d8ab752cd086d
Jad AlHamwi <j.hamwi@centroidsol.com>
2025-01-08 21:00:46 +0400

    compose update

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
+USERNAME=$1
+PASSWORD=$2
+DB_URL="mongodb://$USERNAME:$PASSWORD@offerly-db:27017/offerly"
+DATA_DIR='/offerly-data'
+UPLOADS_DIR='/offerly-uploads'
+COUNTRIES='["Saudi Arabia"]'
+CATEGORIES='["Shopping","Travel","Restaurants & Cafes","Entertainment","Car Services","Health & Wellness","Others","Groceries"]'
+LANGUAGES='["ar","en"]'
 sh ./pull.sh
 docker compose up --build -d
