6a4f8e1fe56e500c947544ab3c4110beabbf8b00
Jad AlHamwi <j.hamwi@centroidsol.com>
2025-01-12 18:18:55 +0400

    cert-renew

--- cert-renew.sh
+++ cert-renew.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 logfile="/logs/cert-renew.log"
 current_date=$(date '+%Y-%m-%d %H:%M:%S')
 docker_command_output=$(docker compose -f /root/offerly-ops/docker-compose.yaml run --rm certbot renew 2>&1)
