e2078f3b7a73e72961ee124d7f2baae974342266
Jad AlHamwi <j.hamwi@centroidsol.com>
2025-01-12 18:30:28 +0400

    db backup script

---
+++ db-backup.sh
@@ -0,0 +1,24 @@
+backup_date=$(date '+%Y-%m-%d_%H-%M-%S')
+backup_name="$backup_date.backup"
+backup_path="./backups/db"
+
+docker exec offerly-db-c mongodump --uri="$MONGO_URI" --out "/data/db/backup/$backup_name"
+
+
+if [ $? -eq 0 ]; then
+  echo "Offerly DB Backup - $backup_name - Completed"
+else
+  echo "Offerly DB Backup - $backup_name - Failed"
+  exit 1 
+fi
+
+
+docker cp "offerly-db-c:/data/db/backup/$backup_name/" "$backup_path"
+
+
+if [ $? -eq 0 ]; then
+  echo "Backup $backup_name copied to host successfully."
+else
+  echo "Error copying backup $backup_name to host."
+  exit 1 
+fi
